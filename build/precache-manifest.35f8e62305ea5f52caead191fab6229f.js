f8b620f57bbf7970485ca1440e30aeadd0f73c60
Spelkington <spelkington@gmail.com>
2019-10-22 00:59:00 -0600

    first build

---
+++ build/precache-manifest.35f8e62305ea5f52caead191fab6229f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83d963574944168db0c7cdb78c93c865",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30a575c233dc7191d247",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6106937ad7687365c8d3",
+    "url": "/static/js/2.50da94e9.chunk.js"
+  },
+  {
+    "revision": "30a575c233dc7191d247",
+    "url": "/static/js/main.8eee6149.chunk.js"
+  },
+  {
+    "revision": "e3b8194bf1e5c7ab91f8",
+    "url": "/static/js/runtime-main.4741a1ba.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
